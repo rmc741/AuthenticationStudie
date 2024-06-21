dea32c0d4ada8bdda2dff3b563f496893b5be226
Rafael Mendonca <rafael.medonca2@gmail.com>
2024-06-21 16:42:36 -0300

    adiciona biblioteca do entity e classe de context

---
+++ AuthenticationStudie/Entity/OurHeroDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationStudie.Entity
+{
+    public class OurHeroDbContext : DbContext
+    {
+        public OurHeroDbContext(DbContextOptions<OurHeroDbContext> options) : base(options)
+        {
+        }
+    }
+}
