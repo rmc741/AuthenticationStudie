6ed6c5ae26d49c022923e15fd0db203300ec54ac
Rafael Mendonca <rafael.medonca2@gmail.com>
2024-06-24 09:41:11 -0300

    migrations

---
+++ AuthenticationStudie/Migrations/20240624123536_PrimeiraMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AuthenticationStudie.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeiraMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OurHeros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OurHeros", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "OurHeros",
+                columns: new[] { "Id", "FirstName", "IsActive", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "System", true, "" },
+                    { 2, "Teste", true, "Last Name" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OurHeros");
+        }
+    }
+}
